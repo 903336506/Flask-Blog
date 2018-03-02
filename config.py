2a0bf7ed60d2af81cc5a8cad2e040644d09441d0
李文毅 <liwenyi@jiagouyun.com>
2018-03-02 18:58:14 +0800

    数据库完成

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 class Config(object):
     """Base config class."""
     pass
@@ -10,3 +11,5 @@ class DevConfig(Config):
     """Development config class."""
     # Open the DEBUG
     DEBUG = True
+    # MySQL connection
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:fanguiju@127.0.0.1:3306/myblog?charset=utf8'
